50ce8025810d08538e00d931832f440652a9ae64
vramaiah <>
2022-12-08 17:54:22 +0000

    Refactor and clean up front-end

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -17,9 +17,8 @@ function get_random_city() {
         // handle a non-successful response
         error : function(xhr,errmsg,err) {
             $("#loading-div").remove(); // remove the loading sign
-            var error_box = $("<div data-alert>")
-            error_box.attr("class", "alert-box alert radius").text("Oops! We have encountered an error: "+errmsg+
-                " <a href='#' class='close'>&times;</a>").appendTo("#results") // add the error to the dom
+            var error_box = $("<div data-alert>");
+            error_box.attr("class", "alert-box alert radius").html("Oops! We have encountered an error: "+errmsg).appendTo("#results"); // add the error to the dom
             console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
         }
     });
